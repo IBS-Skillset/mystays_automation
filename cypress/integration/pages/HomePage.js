a3084409f25e50fef1ac68779b74352572da3dd3
sovarghese <112616349+sovarghese@users.noreply.github.com>
2022-10-12 19:42:47 +0530

    Revert "homepage_validation"

--- cypress/integration/pages/HomePage.js
+++ cypress/integration/pages/HomePage.js
@@ -29,26 +29,8 @@ class HomePage{
     }
     verifyHomePage(){
         cy.url().should('include', 'http://localhost:3000/')
-        cy.visit('http://localhost:3000')
 
     }
-    verifyDestinationField(){
-        this.elements.destinationField().should('be.visible').and('be.enabled')
-
-    }
-    verifyCalenderField(){
-        this.elements.calendarIcon().should('be.visible')
-
-    }
-    verifySearchButton(){
-        this.elements.searchButton().should('be.visible').and('be.enabled')
-
-    }
-    verifyLanguageSelectionField(){
-        this.elements.languageSelectButton().should('be.visible').and('be.enabled')
-
-    }
-
 }
 
 export default HomePage
